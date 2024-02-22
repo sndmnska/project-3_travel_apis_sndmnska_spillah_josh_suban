d476c1994f486ee9d0f42c529cf1266cf3a983ff
sndmnska <102878282+sndmnska@users.noreply.github.com>
2024-02-22 11:10:55 -0600

    Add message method

--- ui.py
+++ ui.py
@@ -3,3 +3,8 @@ UI - User Interface
 
 This module is used to accept inputs and display outputs to the user.
 """
+
+def message(msg):
+    """ Prints a message for the user
+     :param msg: the message to print"""
+    print(msg)
