22a94d12e560c5728388f991b32668f6cea1bba6
Josh Mathews <144305492+JoshuaMathews@users.noreply.github.com>
2024-03-01 16:14:27 -0600

    Use ui.py message, swap recursion for while loops, updated error handling for no api key or internet, and split geo method into two methods

--- api_travel_advisory.py
+++ api_travel_advisory.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from ui import message
 
 # Grab key from env variables.
 api_key = os.environ.get('API_KEY')
@@ -9,10 +10,16 @@ assert api_key is not None
 
 def main():
     city = get_city()
-    country_code = get_geo_data(city)
+    geo_data = geo_request(city)
+
+    if geo_data is None:
+        message(f'Unable to find {city}.')
+        return
+
+    country_code = get_country_code(geo_data)
 
     if country_code is None:
-        print(f'Unable to find {city}.')
+        message(f'Unable to find {city}.')
         return
 
     params = {'countrycode': country_code}
@@ -23,10 +30,10 @@ def main():
     country_advisory = country_data['advisory']
 
     # Let me know if the tabs are a good idea or not, to me it makes it feel like its more grouped.
-    print(f'The advisory for {country_data['name']}:')
-    print(f'\t {country_advisory['message']}')
-    print(f'\t Last updated: {country_advisory['updated']} UTC')
-    print(f'\t Source: {country_advisory['source']}')
+    message(f'The advisory for {country_data['name']}:')
+    message(f'\t {country_advisory['message']}')
+    message(f'\t Last updated: {country_advisory['updated']} UTC')
+    message(f'\t Source: {country_advisory['source']}')
 
 def handle_request(url, params):
     '''
@@ -43,33 +50,51 @@ def handle_request(url, params):
         return None
 
 
-def get_geo_data(city):
+def geo_request(city):
     '''
     Converts user inputted city into country code.
     :param city: User's City.
-    :return:
+    :return: Dict of user's geo request or None if not found.
     '''
     params = {'q': city, 'limit': '1', 'APPID': api_key}
     url = 'http://api.openweathermap.org/geo/1.0/direct'
-    data = handle_request(url, params)
-
-    if len(data) == 0 or data is None:
-        return None
+    data = None
 
     try:
-        return data[0]['country']
-    except Exception as e:
-        print(e)
+        data = handle_request(url, params)
+    except requests.exceptions.ConnectionError as e:
+        message('Please check your internet connection!')
+        return None
+    except Exception:
+        message('An Unknown error has occurred!')
+        return None
+
+    if not data or len(data) == 0:
+        return None
+    else:
+        return data
+
+
+def get_country_code(data):
+    '''
+    :param data: Dict of user's chosen city geo data.
+    :return: String of ISO-2 country code or None if invalid.
+    '''
+
+    if not data or len(data) == 0 or not data[0] or not data[0]['country']:
+        return None
+
+    return data[0]['country']
 
 def get_city():
     '''
     Gets city from user's input.
     '''
-    city = input('Input your chosen city: ')
 
-    if not city or len(city) == 0 or not isinstance(city, str):
-        print("Input correct city please.")
-        return get_city()
+    city = None
+
+    while not city or len(city) == 0 or not isinstance(city, str):
+        city = input('Input your chosen city: ')
 
     return city.capitalize()
 
