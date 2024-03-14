e410d4ede0ca9810df521be8b757bfa070f8c4f9
sndmnska <102878282+sndmnska@users.noreply.github.com>
2024-03-14 17:10:38 -0500

    working on basic program structure

--- api_things_to_do.py
+++ api_things_to_do.py
@@ -7,41 +7,80 @@ Examples:
 
 Search for music events in the Los Angeles area
  https://app.ticketmaster.com/discovery/v2/events.json?
-classificationName=music&dmaId=324&apikey=0tWsbiMH7lHmQdr7K1P7vfUMNWew
-iJWQ
+classificationName=music&dmaId=324&apikey=${API_KEY}
 Get a list of all events for Adele in Canada
  https://app.ticketmaster.com/discovery/v2/events.json?
-attractionId=K8vZ917Gku7&countryCode=CA&apikey=0tWsbiMH7lHmQdr7K1P7vfU
-MNWewiJWQ
+attractionId=K8vZ917Gku7&countryCode=CA&apikey=${API_KEY}
 
 All key-value pairs are delineated by '&'.  Start with the '*.json' and '?'
 
-:Input: latlong from api_geolocation
+:input: city_name from api_geolocation
 :output: str name of the top (1) event returned 
 """
 
 
-latlong = []
+"""
+NO API KEY TREE: # TODO Use for error handling
+{
+    "fault": {
+        "faultstring": "Invalid ApiKey",
+        "detail": {
+            "errorcode": "oauth.v2.InvalidApiKey"
+        }
+    }
+}
+"""
 
-root_url = 'https://app.ticketmaster.com/discovery/v2/'
+import requests, os
 
-# FIXME - Delete these lines when ready to test with api_geolocation.py
-# For testing purposes, assign a known working lat_long tuple. 
-sample_latlong = [44.9772995, -93.2654692]  # Minneapolis, MN, USA
-latlong = sample_latlong
+city_name = ''
 
+root_url = 'https://app.ticketmaster.com/discovery/v2/events'
+ticketmaster_api_key = os.environ["TICKETMASTER_KEY"]
 
-def get_request_from_latlong(latlong):
+def import_city_name():
+    '''
+    Get city name from geolocation search
+    :output: [str] city name
+    '''
+
+    # TODO FIXME - Delete these next few lines when ready to test with api_geolocation.py
+    # For testing purposes, assign a known working city name. 
+    sample_city = 'Minneapolis'  # Minneapolis, MN, USA [44.9772995, -93.2654692]
+    city_name = sample_city
+
+    return city_name
+
+def get_request_from_city_name(city_name):
+    '''
+    Request a json response from the Ticketmaster API.   
+        Convert response to dictionary, or send errors as applicable.
+    :input: city_name
+    :outputs: [dict] response, [dict] error information
+    '''
+
     pass
 
-def get_top_event_from_response(response):
+def get_top_event_from_response(response):    
+    '''
+    Extract the top event from a successful response.
+        Send error if unexpected structure
+    :input: response dictionary
+    :outputs: [dict] event key:value pair, [dict] error information
+    '''
     pass
 
 def convert_event_to_str(event):
+    '''
+    :input: [dict] event key:value pair
+    :output: [str] event name, capitalized   
+    '''
     return string
 
 def main():
-    get_request_from_latlong(latlong)
+    city_name = import_city_name() 
+
+    get_request_from_city_name(city_name)
     get_top_event_from_response(response)
     event_str = convert_event_to_str(event)
     # return event_str to api_handler -> main.py
