79ac7b8724d2389847bb3a32c4197103d3c782f9
Your Name <you@example.com>
2024-03-16 15:32:29 -0500

    Yelp API where to eat foods

--- db_handler.py
+++ db_handler.py
@@ -49,3 +49,21 @@ def create_table(): # to check if table exists
         with sqlite3.connect('records_db.sqlite') as conn:
             conn.execute("INSERT INTO geo_location VALUES (?,?,?)", (destination, latitude,longitude))
         conn.close()
+
+# OLD CODE,
+# 
+# def create_table():
+#     # Create table if is not exists
+#     with sqlite3.connect('records_db.sqlite') as conn:
+#         conn.execute('CREATE TABLE IF NOT EXISTS restaurants (name TEXT, city TEXT, country TEXT, address TEXT)')
+#     conn.close()
+
+
+# def add_restaurant_data(name, city, country, address):
+
+#     create_table()
+
+#     with sqlite3.connect('records_db.sqlite') as conn:
+#         conn.execute("INSERT INTO restaurants VALUES (?, ?, ?, ?)", (name, city, country, address))
+#     conn.close()
+
--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 """
-Project 3 - A Useful Travel App (working title)
+This is main section
+"""
+from ui import message
+from data_presenter import print_businesses
+from api_food_to_eat import get_restaurants_for_location
+
+
+def main():
+
+    city = input('Enter the name of the city: ').strip()
+    country = input('Enter the 2-letter country code: ').strip().upper()
+
+    location = f"{city}, {country}"
+    restaurants = get_restaurants_for_location(location)
+
+    if restaurants:
+        for restaurant in restaurants:
+            print_businesses(restaurant)
+            # add_restaurant_data(restaurant)
+
+            make_choice = input("Press Enter to continuous or 'q' to quit: ")
+            if make_choice.lower() == 'q':
+                message("Thanks, goodbye.")
+                break
+    else:
+        message('No restaurants found in the given location.')
 
 
 """
