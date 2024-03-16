96dfc8fed262b62cde6ee0ff28ecc1f1f572d6ef
Your Name <you@example.com>
2024-03-15 21:38:10 -0500

    Yelp API where to eat foods

--- api_food_to_eat.py
+++ api_food_to_eat.py
@@ -1,74 +1,33 @@
-from pprint import pprint
-
-import requests
+from api_handler import API_request
+from ui import message
 import os
 
 # Yelp URL for the business search endpoint
-url = 'https://api.yelp.com/v3/businesses/search'
-key = os.environ.get('YELP_API_KEY')
+url = 'https://api.yelp.com/v3/businesses/search' 
+key = os.environ.get('YELP_API_KEY')  
+headers = {
+        'accept': 'application/json', 
+        'Authorization': f'Bearer {key}' 
+   }  
+   
+#This is the functions to request data from YELP API 
+def get_restaurants_for_location(location): 
 
+    # call function(s) that make API call
+    # return data about restaurants
+    params = {'term': 'Restaurants', 'location': location}
+    response = API_request(url, headers=headers, params=params)
 
-def main():  # Main function for the program
-    location = get_location() # Gets the user location input
-    response, error = make_api_request(location, key)  # Handle exceptions in program
+    if response is None:
+        message(f"Failed to make API request for {location}")
+        return None
+    
+    if 'businesses' not in response:
+        return None
 
-    if error:
-        print(error)
-        print('Sorry, Somthing went wrong')
-        return
+    restaurants = response['businesses']
 
-    if response: # this is the response of the program
-        restaurant_address = get_restaurant_from_api_response(response)
-        if restaurant_address:
-            print_businesses(restaurant_address)
-        else:
-            print('No location found.')
-
-
-def get_location(): # This function gets the location for user
-    city, country = '', ''
-    while len(city) == 0:
-        city = input('Enter the name of the city: ').strip()
-
-    while len(country) != 2 or not country.isalpha():
-        country = input('Enter the 2-letter country code: ').strip()
-
-    location = f'{city}, {country}'  # This is formation of the location
-    return location
-
-# This is the functions to request data from YELP API
-def make_api_request(location, key):
-    try:
-        headers = {
-            'accept': 'application/json',
-            'Authorization': f'Bearer {key}'
-        }
-        # This founds runts that is in the location user input
-        params = {'term': 'Restaurants', 'location': location}
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()  # Raise exception
-        data = response.json()   # this may error too, if response is not JSON
-        return data, None
-    except requests.RequestException as e:
-        return None, f"Error making API request: {e}"
-
-
-# this function gets the restaurant information from the yelp API
-def get_restaurant_from_api_response(response):
-    businesses = response.get('businesses', [])
-    return businesses
-
-
-# Gets each restaurant from the data
-def print_businesses(businesses):
-    for restaurant in businesses:
-        name = restaurant.get('name')
-        city = restaurant['location'].get('city')
-        country = restaurant['location'].get('country')
-        address = restaurant['location'].get('address1')
-        print(f"Name: {name}, City: {city}, Country: {country}, Address: {address}")
-
-
-if __name__ == "__main__":
-    main()
-
+    if not restaurants:
+        message(f"No restaurants fount in this location: {location}")
+    else:
+        return restaurants
--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 """
-Project 3 - A Useful Travel App (working title)
-
-
+This is main section
 """
+from ui import message
+from api_food_to_eat import get_restaurants_for_location
+
+def main():
+    city = input('Enter the name of the city: ').strip()
+    country = input('Enter the 2-letter country code: ').strip().upper()
+
+    location = f"{city}, {country}"
+    restaurants = get_restaurants_for_location(location)
+
+    if restaurants:
+        for restaurant in restaurants:
+            name = restaurant.get('name')
+            city = restaurant['location'].get('city')
+            country = restaurant['location'].get('country')
+            address = restaurant['location'].get('address1')
+            message(f"Name: {name}, City: {city}, Country: {country}, Address: {address}")
+    else:
+        message('No restaurants found in the given location.')
+
+
+if __name__ == "__main__":
+    main()
+
