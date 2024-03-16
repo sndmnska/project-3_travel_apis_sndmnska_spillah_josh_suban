1a084639f1192bbb9dfa0864f45cec91a665f8ed
sndmnska <102878282+sndmnska@users.noreply.github.com>
2024-03-16 16:15:03 -0500

    Incorporate UI functionality
    Replaced input() with ui.user_question()

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 """
 This is main section
 """
-from ui import message
+from ui import message, user_question
 from data_presenter import print_businesses
 from api_food_to_eat import get_restaurants_for_location
 
 
 def main():
 
-    city = input('Enter the name of the city: ').strip()
-    country = input('Enter the 2-letter country code: ').strip().upper()
+    city = user_question('Enter the name of the city: ').strip()
+    country = user_question('Enter the 2-letter country code: ').strip().upper()
 
     location = f"{city}, {country}"
     restaurants = get_restaurants_for_location(location)
@@ -19,7 +19,7 @@ def main():
             print_businesses(restaurant)
             # add_restaurant_data(restaurant)
 
-            make_choice = input("Press Enter to continuous or 'q' to quit: ")
+            make_choice = user_question("Press Enter to continuous or 'q' to quit: ")
             if make_choice.lower() == 'q':
                 message("Thanks, goodbye.")
                 break
