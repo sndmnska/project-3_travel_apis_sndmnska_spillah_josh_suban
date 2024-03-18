a4f0f9935d244cbfc9c12d9944a9e5ae3f09c9bb
FIRST_NAME LAST_NAME <example@example.com>
2024-03-18 10:24:35 -0500

    added functions and created tables if they do not exist to store data

--- db_handler.py
+++ db_handler.py
@@ -1,5 +1,51 @@
 """
 DB Handler
 
-This module communicates between the main.py program and the SQLite database
+This module communicates between the main.py program and the SQLite database to process and display data in a user frienly manner
 """
+import sqlite3
+
+def create_table(): # to check if table exists
+    #creates table if table does not exists
+    with sqlite3.connect('records_db.sqlite') as conn:
+        conn.execute('CREATE TABLE IF NOT EXISTS food_to_eat(country TEXT, food_name TEXT, description TEXT')
+        conn.execute('CREATE TABLE IF NOT EXISTS travel_advisory(country TEXT, advisory TEXT')
+        conn.execute('CREATE TABLE IF NOT EXISTS things_to_do(destination TEXT, activity TEXT, description TEXT ')
+        conn.execute('CREATE TABLE IF NOT EXISTS geo_location(destination TEXT, latitude TEXT, longtitude TEXT')
+    conn.close()
+
+# functions to put data in the right tables in the SQLITE database
+    def add_restaurant_data(name, city, country, address):
+        create_table()
+        with sqlite3.connect('records_db.sqlite') as conn:
+            conn.execute("INSERT INTO restaurants VALUES (?, ?, ?, ?)", (name, city, country, address))
+        conn.close()
+
+    def add_food_to_eat(country, food_name,description):
+        create_table
+        with sqlite3.connect('records_db.sqlite') as conn:
+            conn.execute("INSERT INTO food_to_eat VALUES (?,?,?)", (country, food_name, description))
+        conn.close()
+
+
+    def add_travel_advisory( country, advisory):
+        create_table()
+        with sqlite3.connect('records_db.sqlite') as conn:
+            conn.execute("INSERT INTO travel_advisory VALUES (?,?,)", (country, advisory))
+        conn.close()
+
+
+    def add_things_to_do(destination, activivty, description):
+        create_table()
+        with sqlite3.connect('records_db.sqlite') as conn:
+            conn.execute("INSERT INTO things_to_do VALUES (?,?,?)", (destination, activivty,description))
+
+        conn.close()
+
+
+
+    def add_geo_location(destination, latitude,longitude):
+        create_table()
+        with sqlite3.connect('records_db.sqlite') as conn:
+            conn.execute("INSERT INTO geo_location VALUES (?,?,?)", (destination, latitude,longitude))
+        conn.close()
