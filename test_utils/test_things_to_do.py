641a6adcc3d04e345141fa0fb03008473e5edf81
SndMnSka <dsmeiska@gmail.com>
2024-03-19 18:37:43 -0500

    structure blank methods for testing using TestCase

---
+++ test_utils/test_things_to_do.py
@@ -0,0 +1,22 @@
+import api_things_to_do
+from unittest import TestCase
+from unittest.mock import patch, call, Mock
+
+class TestThingsToDo(TestCase):
+    def test_get_events_request_from_city_name_response_ok(self):
+        pass
+    
+    def test_get_events_request_from_city_name_bad_response(self):
+        pass
+    
+    def test_random_event_from_response_expected_dict(self):
+        pass
+
+    def test_random_event_from_response_KeyError(self):
+        pass
+    
+    def test_convert_event_to_strings_expected_dict(self):
+        pass
+
+    def test_convert_event_to_strings_KeyError(self):
+        pass
